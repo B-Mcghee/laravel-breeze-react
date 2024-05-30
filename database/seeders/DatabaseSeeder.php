f2860b7222a71833a9fd88f12e6d4bbbe836f729
Brandon McGhee <Bmcghee.dev@gmail.com>
2024-05-30 05:36:39 -0400

    initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'bmcghee39@gmail.com'],
+            ['name' => 'Brandon', 'password' => Hash::make('testing123!')]
+        );
     }
 }
